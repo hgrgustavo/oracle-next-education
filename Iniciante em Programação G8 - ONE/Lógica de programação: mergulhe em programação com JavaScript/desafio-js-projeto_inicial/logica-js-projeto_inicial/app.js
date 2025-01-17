a3a4d89146fdfb59355f737bf8fc9b605a73df7b
gus <gustavohgrdev@gmaiil.com>
2025-01-17 00:41:39 -0300

    progress in programming logic with javascript

---
+++ Iniciante em Programação G8 - ONE/Lógica de programação: mergulhe em programação com JavaScript/desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,18 @@
+alert("Boas vindas ao nosso site!");
+alert("Erro! Preencha todos os campos");
+alert(mensagemDeErro);
+
+
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = "Erro! Preencha todos os campos";
+
+
+prompt("Qual o seu nome de usuário?") = nome;
+prompt("Qual a sua idade?") = idade;
+
+if (idade >= 18) {
+    alert("Pode tirar a habilitação!");
+}
